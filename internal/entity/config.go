b4a31b8892da36bda35c735408b85d90b9652456
Sergey Zaytsev <sergey.zaytsev@varargs.ru>
2021-11-21 22:50:08 +0300

    added new fields in config

--- internal/entity/config.go
+++ internal/entity/config.go
@@ -1,7 +1,15 @@
 package entity
 
+import "time"
+
 type Config struct {
-	DebugMode     bool   `env:"DEBUG_MODE,default=true"`
-	Port          string `env:"LISTEN_PORT"`
-	TelegramToken string `env:"TELEGRAM_TOKEN"`
+	Debug bool `env:"DEBUG,default=true"`
+
+	ListenPort   string        `env:"LISTEN_PORT,default=80"`
+	ReadTimeout  time.Duration `env:"READ_TIMEOUT,default=15"`
+	WriteTimeout time.Duration `env:"WRITE_TIMEOUT,default=15"`
+	IdleTimeout  time.Duration `env:"IDLE_TIMEOUT,default=60"`
+
+	Token string `env:"TELEGRAM_BOT_TOKEN"`
+	Hook  string `env:"TELEGRAM_HOOK_LINK"`
 }
